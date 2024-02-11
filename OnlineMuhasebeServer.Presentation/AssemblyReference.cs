2ed25dc6cd157cc53bbb1c4ea103b539da983e42
kilicseyit <seyit.kilic1@outlook.com>
2024-02-11 16:51:41 +0300

    Proje dosyası ekle.

---
+++ OnlineMuhasebeServer.Presentation/AssemblyReference.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+namespace OnlineMuhasebeServer.Presentation
+{
+    public static class AssemblyReference
+    {
+        //burada taşınan katmanın assebmlysini aktardık
+        public static readonly Assembly Assembly = typeof(Assembly).Assembly;
+    }
+}
