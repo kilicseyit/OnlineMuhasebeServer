8e51f2436edcf357a9bf0e33cf77e28b670aa3bf
kilicseyit <seyit.kilic1@outlook.com>
2024-02-12 14:35:33 +0300

    değişiklik yapıldı

--- OnlineMuhasebeServer.WebApi/Program.cs
+++ OnlineMuhasebeServer.WebApi/Program.cs
@@ -1,14 +1,14 @@
-using Microsoft.OpenApi.Models;
-using System.Reflection.Metadata;
-using static System.Net.WebRequestMethods;
-using System.Reflection.PortableExecutable;
-using System.Security.Cryptography.Xml;
-using System.Xml.Linq;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using OnlineMuhasebeServer.Persistance.Context;
+using System.Reflection.Metadata;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container.,
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
 
 builder.Services.AddControllers()
     .AddApplicationPart(typeof(AssemblyReference).Assembly);//burada aktarma iþlemini yaptýk
